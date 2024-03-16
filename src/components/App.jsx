0908a675375e1be1e2046da292fd3692f7b3c56e
Annargb <annasamoylenko2015@gmail.com>
2024-03-16 17:31:43 +0100

    add base

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { AppLayout } from "./AppLayout/AppLayout";
+import GlobalStyle from "./GlobalStyle";
+
+export const App = () => {
+  return (
+    <>
+      <AppLayout />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
